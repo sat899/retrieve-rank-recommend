757200c80c0f900f9c40a8ec917260ba03cda2da
sat899 <satyam.bhagwanani@gmail.com>
2026-09-12 09:11:22 +0100

    adding function to normalize 1D vectors

--- utils/linalg.py
+++ utils/linalg.py
@@ -11,3 +11,14 @@ def compute_vector_magnitude(vector: np.ndarray) -> float:
     Equivalent to np.linalg.norm in numpy.
     """
     return float(np.sqrt(np.sum(vector ** 2)))
+
+def normalize_vector_length(vector: np.ndarray) -> np.ndarray:
+    """
+    Normalizes a 1D vector by its Euclidean length (L2 norm) to unit length (1)
+    """
+    vector_magnitude = compute_vector_magnitude(vector)
+    
+    if vector_magnitude == 0.0:
+        return vector.astype(float)
+    else:
+        return vector / vector_magnitude
